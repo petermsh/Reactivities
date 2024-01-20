cac4c203baf4f80cbdccd5510d32659dca716aff
petermsh <aron17101@gmail.com>
2024-01-20 21:01:39 +0100

    Section 11

---
+++ client-app/src/app/common/options/categoryOptions.ts
@@ -0,0 +1,8 @@
+﻿export const categoryOptions = [
+    {text: 'Drinks', value: 'drinks'},
+    {text: 'Culture', value: 'culture'},
+    {text: 'Film', value: 'film'},
+    {text: 'Food', value: 'food'},
+    {text: 'Music', value: 'music'},
+    {text: 'Travel', value: 'travel'},
+]
